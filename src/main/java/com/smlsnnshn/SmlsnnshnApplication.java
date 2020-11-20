fc30881f7f81329c3bafe2fd517f2c2f86a9874d
ismailsinansahin <ismailsinansahin@gmail.com>
2020-11-20 14:48:30 +0300

    First Commit

---
+++ src/main/java/com/smlsnnshn/SmlsnnshnApplication.java
@@ -0,0 +1,13 @@
+package com.smlsnnshn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmlsnnshnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmlsnnshnApplication.class, args);
+    }
+
+}
